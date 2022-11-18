94d467dd5886d039a16ee643a46f14c0583c7139
Andrew Sow <asojischool@gmail.com>
2022-11-18 14:54:51 +0800

    Update splashscreen with fonts and logo

---
+++ app/src/main/java/com/example/ande_assignment_budget/Splashscreen.java
@@ -0,0 +1,37 @@
+package com.example.ande_assignment_budget;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Launch the layout -> splash.xml
+        setContentView(R.layout.splash);
+        Thread splashThread = new Thread() {
+
+            public void run() {
+                try {
+                    // sleep time in milliseconds (3000 = 3sec)
+                    sleep(4000);
+                }  catch(InterruptedException e) {
+                    // Trace the error
+                    e.printStackTrace();
+                } finally
+                {
+                    // Launch the MainActivity class
+                    Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+        };
+        // To Start the thread
+        splashThread.start();
+    }
+}
