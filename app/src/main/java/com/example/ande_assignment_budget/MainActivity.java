f61653e4a7702aba10e52c29b56c5d7c6b737833
Andrew Sow <asojischool@gmail.com>
2022-11-19 22:52:34 +0800

    Add datepicker and change some color in colors.xml

--- app/src/main/java/com/example/ande_assignment_budget/MainActivity.java
+++ app/src/main/java/com/example/ande_assignment_budget/MainActivity.java
@@ -4,22 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ande_assignment_budget.Model.CategoryModel;
+import com.whiteelephant.monthpicker.MonthPickerDialog;
 
+import java.text.DateFormatSymbols;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ArrayList<CategoryModel> categoryModels;
+    private MonthPickerDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        monthYearPickerDialog();
         setUpCategoryModels();
         setRecyclerList();
     }
@@ -37,5 +48,45 @@ public class MainActivity extends AppCompatActivity {
         CategorySpent_RecyclerViewAdapter adapter = new CategorySpent_RecyclerViewAdapter(this, categoryModels);
         rvCategorySpent.setAdapter(adapter);
         rvCategorySpent.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void monthYearPickerDialog() {
+        Calendar today = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        changeDate(dateToString(today.get(Calendar.MONTH)), today.get(Calendar.YEAR));
+
+        builder = new MonthPickerDialog.Builder(MainActivity.this,
+            new MonthPickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(int selectedMonth, int selectedYear) {
+                    changeDate(dateToString(selectedMonth), selectedYear);
+                }
+            }, today.get(Calendar.YEAR), today.get(Calendar.MONTH)
+        );
+
+        builder.setActivatedMonth(Calendar.JULY)
+                .setMinYear(1990)
+                .setActivatedYear(today.get(Calendar.YEAR))
+                .setMaxYear(today.get(Calendar.YEAR))
+                .setActivatedMonth(today.get(Calendar.MONTH))
+                .setTitle("Select month");
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()) {
+            case R.id.btnDatePicker:
+                builder.build().show();
+                break;
+        }
+    }
+
+    public void changeDate(String selectedMonth, int selectedYear) {
+        Button btnDatePicker = findViewById(R.id.btnDatePicker);
+        btnDatePicker.setText(selectedMonth + " " + selectedYear);
+    }
+
+    public String dateToString(int month) {
+        return new DateFormatSymbols().getMonths()[month];
     }
 }
