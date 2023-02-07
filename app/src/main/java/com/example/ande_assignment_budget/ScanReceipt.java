2c7de6765407f8dbcec28b496e7f275ab2038eb2
PYiJie <YIJIE.21@ichat.sp.edu.sg>
2023-02-07 09:51:26 +0800

    add camera function

--- app/src/main/java/com/example/ande_assignment_budget/ScanReceipt.java
+++ app/src/main/java/com/example/ande_assignment_budget/ScanReceipt.java
@@ -1,31 +1,52 @@
 package com.example.ande_assignment_budget;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.IOException;
 
 public class ScanReceipt extends AppCompatActivity implements View.OnClickListener{
+    Button button4;
+    private static final int REQUEST_CAMERA_CODE = 100;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_receipt);
+
+        button4 = findViewById(R.id.button4);
 
         Spinner dropdown = findViewById(R.id.spinner);
 
@@ -34,26 +55,66 @@ public class ScanReceipt extends AppCompatActivity implements View.OnClickListen
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
 
-        setBottomNavigationBar();
-    }
+        if (ContextCompat.checkSelfPermission(ScanReceipt.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ScanReceipt.this, new String[]{
+                    Manifest.permission.CAMERA
+            }, REQUEST_CAMERA_CODE);
+        }
 
-    public void setBottomNavigationBar() {
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setSelectedItemId(R.id.miBudget);
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        button4.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Intent i;
-                switch(item.getItemId()) {
-                    case R.id.miSetting:
-                        i = new Intent(ScanReceipt.this, Setting.class);
-                        startActivity(i);
-                        break;
-                }
-                return true;
+            public void onClick(View view) {
+                CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(ScanReceipt.this);
             }
         });
+
+//        button_copy.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                    String scanned_text = textview_data.getText().toString();
+//                    copyToClipboard(scanned_text);
+//            }
+//        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if(resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
+                    getTextFromImage(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void getTextFromImage(Bitmap bitmap) {
+        TextRecognizer recognizer = new TextRecognizer.Builder(this).build();
+        if (!recognizer.isOperational()) {
+            Toast.makeText(ScanReceipt.this, "Error Occurred", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> textBlockSparseArray = recognizer.detect(frame);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < textBlockSparseArray.size(); i++) {
+                TextBlock textBlock = textBlockSparseArray.valueAt(i);
+                stringBuilder.append(textBlock.getValue());
+                stringBuilder.append("\n");
+            }
+//            textview_data.setText(stringBuilder.toString());
+            button4.setText("Retake");
+//            button_copy.setVisibility(View.VISIBLE);
+        }
+    }
+
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button2:
@@ -63,5 +124,12 @@ public class ScanReceipt extends AppCompatActivity implements View.OnClickListen
             case R.id.button:
                 break;
         }
+    }
+
+    private void copyToClipboard(String text) {
+        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Copied Data", text);
+        clipboardManager.setPrimaryClip(clip);
+        Toast.makeText(ScanReceipt.this, "Copied to clipboard", Toast.LENGTH_SHORT);
     }
 }
